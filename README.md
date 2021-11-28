6ce187645dd8c0054b78156d9215df5fce172c43
awbt <mail@jhcloud.top>
2021-11-28 17:41:24 +0800

    add example

---
+++ README.md
@@ -0,0 +1,14 @@
+## rust 小练习
+
+### run example
+in windows
+* server
+```
+set RUST_LOG=info
+cargo run --example server --quiet
+```
+* client
+```
+set RUST_LOG=info
+cargo run --example client --quiet
+```
---
+++ examples/server.rs
@@ -0,0 +1,30 @@
+use anyhow::Result;
+use async_prost::AsyncProstStream;
+use futures::prelude::*;
+use kv::{CommandRequest, CommandResponse};
+use tokio::net::TcpListener;
+use tracing::info;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    tracing_subscriber::fmt().init();
+    let addr = "127.0.0.1:9527";
+    let listener = TcpListener::bind(addr).await?;
+    info!("Start listening on {}", addr);
+    loop {
+        let (stream, addr) = listener.accept().await?;
+        info!("Client {:?} connected", addr);
+        tokio::spawn(async move {
+            let mut stream = AsyncProstStream::<_, CommandRequest, CommandResponse, _>::from(stream)
+                .for_async();
+            while let Some(Ok(msg)) = stream.next().await {
+                info!("Got a new command: {:?}", msg);
+                let mut response = CommandResponse::default();
+                response.status = 404;
+                response.message = "page not found".to_string();
+                stream.send(response).await.unwrap();
+            }
+            info!("Client {:?} disconnected", addr);
+        });
+    }
+}
